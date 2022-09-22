52e9ae9f8a84b74d238f486ad11ad09235c1a8a9
Angela <chepkoechangela514@gmail.com>
2022-09-22 07:21:16 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -13,20 +13,31 @@ function Home() {
     <div id="home">
       <h1>Home</h1>
     </div>
-  );
+ );
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
+
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
-  );
-}
 
-export default App;
+
+
+
+
